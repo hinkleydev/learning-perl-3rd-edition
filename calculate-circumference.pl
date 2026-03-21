85b5169135510292cdd847d0204a944057bc321d
hinkleydev <azz@hinkley.dev>
2026-03-21 16:58:16 +0000

    [2] Third step

--- calculate-circumference.pl
+++ calculate-circumference.pl
@@ -4,4 +4,9 @@ $radius = <STDIN>;
 chomp $radius;
 $PI = 3.14; # hey it's only a test
 $circumference = $radius * 2 * $PI;
-print "circumference is $circumference\n";
+if ($circumference > 0) {
+	print "circumference is $circumference\n";
+} else {
+	print "negative value, circumference is 0\n";
+}
+
